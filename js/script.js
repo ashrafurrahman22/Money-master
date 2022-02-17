02739bd43cc739e2f833fd4f28dd38e7a823de50
ashrafurrahman22 <ashrafurrahman22@gmail.com>
2022-02-17 10:01:32 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+document.getElementById('calculate-button').addEventListener('click', function(){
+
+            // get income input and convert to numbers
+            const incomeInput = document.getElementById('income-input');
+            const previousIncomeAmountText = incomeInput.value;
+            const previousIncomeAmount = parseInt(previousIncomeAmountText);
+
+            // get food, rent & others input
+            // food
+            const foodInput = document.getElementById('food-input');
+            const foodAmountText = foodInput.value;
+            const foodAmount = parseInt(foodAmountText);
+
+            // rent
+            
+})
